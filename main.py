33e14c823b2a101e4321282102454bb7467b9557
swaraj25-git <swarajmahale25@gmail.com>
2025-06-27 10:40:35 +0530

    Creating a Window

---
+++ main.py
@@ -0,0 +1,59 @@
+
+
+import turtle as tr
+
+
+
+# print('Hello')
+
+"""
+For breakoit game
+using turtle
+create a window/ screen
+bottom screen a horizontal brick
+it should be moved by pressing left right keys
+a ball
+and bricks at top part of screen
+ball will hit the baricks at top part
+bouncce back and using ledt/right keys move the bottom brick /paddle
+ball wil hit the padle and again bounce back
+
+"""
+
+#####Structure
+
+###
+# ball.py
+# main.py
+# bricks.py
+# ui.py
+# paddle.py
+#scoreboard.py
+# ###
+
+
+
+####    Main code start Here👇    ###
+
+# a turtle window with black bg
+
+win  = tr.Screen()
+win.setup(width=1200, height=600)
+win.bgcolor('Black')
+win.title('BreakOut!')
+
+tr.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
